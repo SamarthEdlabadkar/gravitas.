08efaa09b0a52c6301c88f140e5a701a5c53e20c
SamarthEdlabadkar <samarth.edlabadkar@gmail.com>
2025-10-04 14:39:59 -0400

    topic generation commit

--- server/scripts/categorize.py
+++ server/scripts/categorize.py
@@ -1,4 +1,30 @@
-from bertopic import BERTopic
-from csv import reader
+#from bertopic import BERTopic
+import csv
+import pandas as pd
 
-reader("")
+with open('server/static/publications.csv', mode='r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)  # Automatically reads headers
+    docs = []
+    for row in reader:
+        title = row['Title']
+        link = row['Link']
+
+        docs.append(title)
+
+# # Create and fit the model
+# topic_model = BERTopic(language="english")
+# topics, probs = topic_model.fit_transform(docs)
+
+# # Get an overview of topics
+# print(topic_model.get_topic_info())
+
+# # Show top keywords for topic 1
+# topic_model.get_topic(1)
+
+# topic_model.visualize_topics()
+
+df = pd.DataFrame({
+    "Document": docs,
+    #"Topic": topics
+})
+df.to_csv("server/static/space_biology_topics.csv", index=False)
