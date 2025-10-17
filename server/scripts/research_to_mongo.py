5e8f48841d4846ff4069cd65ee35d598d4751042
Krish Kalathiya <krishkalathiya@gmail.com>
2025-10-17 10:49:40 -0400

    Updated some stuff idk

--- server/scripts/research_to_mongo.py
+++ server/scripts/research_to_mongo.py
@@ -23,9 +23,9 @@ dotenv.load_dotenv()
 
 CHROMA_PATH = "server/static/chroma"
 CHROMA_COLLECTION = "research_papers"
-MONGO_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017')
-MONGO_DB = os.environ.get('MONGO_DB_NAME', 'medosearch')
-MONGO_COLLECTION = os.environ.get('MONGO_COLLECTION_NAME', 'research_papers')
+MONGO_URI = os.environ.get('MONGODB_URI', '')
+MONGO_DB = os.environ.get('MONGO_DB_NAME', 'Gravitas-DB')
+MONGO_COLLECTION = os.environ.get('MONGO_COLLECTION_NAME', 'research-gemini')
 
 
 def iter_collection(client: Any, collection_name: str, batch_size: int = 256) -> List[Dict[str, Any]]:
